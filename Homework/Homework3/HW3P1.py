83bac6d2aaf41559fd6808f9805b9c675d57f97b
simonabrahamse <siab8107@colorado.edu>
2024-09-20 17:34:54 -0600

    Problem 1 added!

--- Homework/Homework3/HW3P1.py
+++ Homework/Homework3/HW3P1.py
@@ -4,14 +4,10 @@ import numpy as np
 def driver():
 
 # test functions 
-     f1 = lambda x: 1+0.5*np.sin(x)
-# fixed point is alpha1 = 1.4987....
+     f1 = lambda x: (np.sin(x)+1)/2
 
-     f2 = lambda x: 3+2*np.sin(x)
-#fixed point is alpha2 = 3.09... 
-
-     Nmax = 100
-     tol = 1e-6
+     Nmax = 1000
+     tol = 1e-8
 
 # test f1 '''
      x0 = 0.0
@@ -19,14 +15,6 @@ def driver():
      print('the approximate fixed point is:',xstar)
      print('f1(xstar):',f1(xstar))
      print('Error message reads:',ier)
-    
-#test f2 '''
-     x0 = 0.0
-     [xstar,ier] = fixedpt(f2,x0,tol,Nmax)
-     print('the approximate fixed point is:',xstar)
-     print('f2(xstar):',f2(xstar))
-     print('Error message reads:',ier)
-
 
 
 # define routines
